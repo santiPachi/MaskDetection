557a8f42c9a04fc40b9a9612f367b73affa33eae
Santi <santiagopacheco@MacBook-Pro-de-Santiago.local>
2020-05-20 14:43:58 -0500

    mdv5

--- app/src/main/java/com/example/maskdetection/GestionMask.java
+++ app/src/main/java/com/example/maskdetection/GestionMask.java
@@ -67,7 +67,7 @@ public class GestionMask {
                 // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
                 String jsonData = response.body().string();
                 JSONObject Jobject = null;
-                System.out.println(jsonData);
+//                System.out.println(jsonData);
                 try {
                     Jobject = new JSONObject(jsonData);
                     String str = Jobject.get("res1").toString()+" "+Jobject.get("res2").toString();
